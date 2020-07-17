60d292cd3dfa09ac010af9f95c3613562a0ca005
Nate Marks <npmarks@gmail.com>
2020-07-17 16:08:17 -0400

    Fix __init__.py with import

--- treecrawl/__init__.py
+++ treecrawl/__init__.py
@@ -4,6 +4,16 @@ __author__ = """Nate Marks"""
 __email__ = "npmarks@gmail.com"
 __version__ = "0.1.1"
 
+
+from .treecrawl import (
+    DirEdit,
+    TestData,
+    file_to_string,
+    find_path_to_ancestor,
+    string_to_file,
+    string_to_log_level,
+)
+
 __all__ = (
     "DirEdit",
     "TestData",
